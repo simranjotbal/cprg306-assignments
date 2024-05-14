d7007493f0af09fcb01418a03508e8eb4555da66
simranjotbal <simranjotkaur.bal@edu.sait.ca>
2024-05-14 12:44:43 -0600

    Created week-2 Assignment
    Successfully done with week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return(
+        <main>
+            <h1>Simranjot Kaur Bal</h1>
+            <Link  className =" underline text-cyan-600 hover:text-pink-300 "href="https://github.com/simranjotbal/cprg306-assignments/commits/master/">Github 
+            </Link>
+        </main>
+    )
+}
